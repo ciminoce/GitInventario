96ea989895e4f218ce03ac503b90ee8324ef5be7
ciminoce <ciminoce@yahoo.com.ar>
2025-01-28 14:04:12 -0300

    Agregar archivos de proyecto.

---
+++ GitInventario.Consola/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitInventario.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
